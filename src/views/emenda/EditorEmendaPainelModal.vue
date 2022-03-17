16749e9067f51ecdfdbc0cb0461b54d0b7162d46
Robson Barros <robson.barros@camara.leg.br>
2022-03-17 23:53:43 +0100

    adicionando modal de visualização do comando de emenda e da ajuda

---
+++ src/views/emenda/EditorEmendaPainelModal.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <painel-modal id="mdlVisualizacao">
+            <template #titulo>
+                Visualização
+            </template>
+            <template #corpo>
+                <lexml-emenda-comando v-if="emenda" :emenda="emenda" />
+            </template>
+        </painel-modal>
+
+        <painel-modal id="mdlAjuda">
+            <template #titulo>
+                Ajuda
+            </template>
+            <template #corpo>
+                <editor-emenda-ajuda
+                    :itens-menu="itensMenu"
+                    :texto-rotulo-dispositivo="textoRotuloDispositivo"
+                />
+            </template>
+        </painel-modal>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { defineAsyncComponent, computed } from 'vue';
+
+const PainelModal = defineAsyncComponent(
+    () => import("../../components/comuns/PainelModal.vue")
+);
+
+const EditorEmendaAjuda = defineAsyncComponent(
+    () => import("./EditorEmendaAjuda.vue")
+);
+
+interface Props {
+    itensMenu: string[];
+    textoRotuloDispositivo: string;
+    emenda?: object;
+}
+
+const props = defineProps<Props>();
+const itensMenu = computed(() => props.itensMenu);
+const emenda = computed(() => props.emenda);
+
+</script>
+
+<style scoped>
+
+</style>
