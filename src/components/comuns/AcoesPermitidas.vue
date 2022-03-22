2cc22faba29d5d57e401da21fef2e9d802e472d5
Erivânio Vasconcelos <erivanio@MacBook-Air-de-Erivanio.local>
2022-03-22 13:38:46 -0300

    Adiciona comentário com TODO nas props emenda e projetoNorma

--- src/components/comuns/AcoesPermitidas.vue
+++ src/components/comuns/AcoesPermitidas.vue
@@ -91,8 +91,8 @@ import { AcaoPermitida, Emenda, Proposicao } from "../../model";
 import { getProposicaoFromObjeto } from '../../utils/typeUtils';
 interface Props {
     item: Proposicao | Emenda;
-    emenda: any | "";
-    projetoNorma: any;
+    emenda: any; // TODO: Adicionar model para lexml-emenda.
+    projetoNorma: any; // TODO: Adicionar model para lexml-projetoNorma.
     acoesPermitidas: Array<AcaoPermitida>;
 }
 const props = defineProps<Props>();
